aa818cd19b04a13d81262e7598fcfdd81fa1d44f
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2022-01-19 17:25:17 -0500

    implemented calculator logic

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let currOperation_div = document.querySelector('.current-operation');
-let prevOperation_div = document.querySelector('.previous-operation');
+let currOperand_div = document.querySelector('.current-operation');
+let prevOperand_div = document.querySelector('.previous-operation');
 let allClear_div = document.querySelector('.clear-all');
 let delete_div = document.querySelector('.delete-one');
 let operands_divs = document.querySelectorAll('.number');
@@ -9,7 +9,9 @@ let decimal_div = document.querySelector('.decimal');
 
 let currOperand = '';
 let prevOperand = '';
+let currOperation = '';
 let result = null;
+let hasDecimal = false;
 
 function add(a, b) {
     return a + b;
@@ -32,27 +34,40 @@ function modulus(a, b) {
 }
 
 function operate(operation, operand1, operand2) {
+    num1 = parseFloat(operand1);
+    num2 = parseFloat(operand2);
+
     if (operation == '/') {
-        result = divide(operand1, operand2);
+        result = divide(num1, num2);
     } else if (operation == 'x') {
-        result = multiply(operand1, operadn2);
+        result = multiply(num1, num2);
     } else if (operation == '+') {
-        result = add(operand1, operand2);
+        result = add(num1, num2);
     } else if (operation == '-') {
-        result = subtract(operand1, operand2);
+        result = subtract(num1, num2);
     } else if (operation == '%') {
-        result = modulus(operand1, operand2);
+        result = modulus(num1, num2);
     }
+    console.log(operation);
+    return String(result.toFixed(2));
 }
 
 function createOperand(num) {
     currOperand += num;
-    currOperation_div.textContent = currOperand;
+    currOperand_div.textContent = currOperand;
 }
 
 allClear_div.addEventListener('click', () => {
-    currOperation_div.textContent = '';
+    currOperand_div.textContent = '';
     currOperand = '';
+    prevOperand_div.textContent = '';
+    prevOperand = '';
+    currOperation = '';
+    hasDecimal = false;
+})
+
+delete_div.addEventListener('click', () => {
+
 })
 
 operands_divs.forEach((num) => {
@@ -61,4 +76,46 @@ operands_divs.forEach((num) => {
     })
 })
 
-console.log(operands_divs[1].textContent)
+decimal_div.addEventListener('click', () => {
+    if (hasDecimal === false) {
+        createOperand(decimal_div.textContent);
+        hasDecimal = true;
+    }
+})
+
+operations_divs.forEach((op) => {
+    op.addEventListener('click', () => {
+        if (prevOperand_div.textContent == '' && currOperation == '') {
+            prevOperand_div.textContent = currOperand;
+            prevOperand = currOperand;
+
+            currOperand_div.textContent = "";
+            currOperand = '';
+
+            currOperation = op.textContent;
+            
+        } else if (prevOperand_div.textContent != '' && currOperand_div.textContent != '' && currOperation != '') {
+            prevOperand = prevOperand_div.textContent;
+            currOperand = currOperand_div.textContent;
+            result = operate(currOperation, prevOperand, currOperand);
+
+            prevOperand_div.textContent = result;
+            prevOperand = result;
+            currOperand = '';
+            currOperand_div.textContent = '';
+
+            currOperation = op.textContent;
+        }
+        hasDecimal = false;
+    })
+})
+
+equals_div.addEventListener('click', () => {
+    if (currOperand == '' || prevOperand == '' || currOperation == '') {
+        return
+    } else {
+        result = operate(currOperation, prevOperand, currOperand);
+    }
+    currOperation = '';
+    currOperand_div.textContent = result;
+})
